dba405d53314b96452aeee3f43c46ee0989713c0
Jesse Newland <jnewland@gmail.com>
2008-01-04 09:41:50 -0500

    license

--- src/gsa-prototype.js
+++ src/gsa-prototype.js
@@ -1,9 +1,8 @@
 /*  gsa-prototype, version 0.0.1
  *  (c) 2008 Jesse Newland
  *  jnewland@gmail.com
- 
- *  gsa-prototype is freely distributable under the terms of an MIT-style license.
  *
+ *  gsa-prototype is freely distributable under the terms of an MIT-style license.
  *--------------------------------------------------------------------------*/
 var Gsa = Class.create({
   initialize: function(domain) {
