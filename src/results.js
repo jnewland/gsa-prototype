19b0c3c8e8f024338d2a881c475ac354e02cf68d
Jesse Newland <jnewland@gmail.com>
2008-01-28 11:33:43 -0500

    results parsing

--- src/results.js
+++ src/results.js
@@ -1,15 +1,43 @@
 Gsa.Results = Class.create({
+  mappings: $H({
+    CRAWLDATE: 'crawlDate',
+    FS: 'details',
+    LANG: 'lang',
+    RK: 'rank',
+    S: 'snippet',
+    T: 'title',
+    UD: 'url',
+    MT: 'meta'
+  }),
+  
   initialize: function(json) {
     this.json = json;
-    this._object = $H();
+    this._object = $H().toObject();
     this.parseJSON();
   },
   
   parseJSON: function () {
-    //this is where the magic happens!
     this.set('query', this.json.Q);
     this.set('time', this.json.TM);
-    this.set('results', []);
+    this.set('start', this.json.RES.SN);
+    this.set('end', this.json.RES.EN);
+    this.set('total', this.json.RES.M);
+    this.set('results', $A(this.json.RES.R).map(function (value){
+      return this.parseResult(value);
+    }.bind(this)));
+  },
+  
+  parseResult: function (r) {
+    r = $H(r);
+    this.mappings.each(function (pair) {
+      var key = pair.key, value = pair.value;
+      if (Object.isString(r.get(key)) || Object.isNumber(r.get(key))) {
+        r.set(value, new String(r.unset(key)).strip());
+      } else {
+        r.set(value, $H(r.unset(key)));
+      }
+    }.bind(r));
+    return r;
   },
   
   _each: function(iterator) {
