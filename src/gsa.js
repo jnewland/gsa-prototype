47f668a28998b0587b8817a82be2d5355eb52455
Jesse Newland <jnewland@gmail.com>
2008-02-01 12:09:16 -0500

    next/previous/page functions

--- src/gsa.js
+++ src/gsa.js
@@ -12,12 +12,14 @@ Gsa = Class.create({
       proxystylesheet: 'json',
       client: 'json',
       site: 'default_collection',
+      start: 0,
       protocol: 'http://'
     }).update(this.parseOptions(options));
 
     //set some properties based on the options
     this.domain = domain;
     this.protocol = this.options.unset('protocol');
+    this.current_page = false;
   },
   
   _request: function(url) {
@@ -41,8 +43,40 @@ Gsa = Class.create({
     this.searchOptions.update(this.options);
     this.searchOptions.set('q', q);
     this.searchOptions.update(this.parseOptions(options));
+    this.current_page = 1;
     this._request(this.buildUri());
     return true;
+  },
+  
+  page: function(page) {
+    if (!Object.isUndefined(this.searchOptions)) {
+      var num = 10;
+      if (!Object.isUndefined(this.searchOptions.get('num')))
+        num = this.searchOptions.get('num');
+      this.searchOptions.set('start', ((page-1)*num));
+      this.searchOptions.update({start: this.searchOptions.get('start')});
+      this.current_page = page;
+      this._request(this.buildUri());
+      return true;
+    } else {
+      return false;
+    }
+  },
+  
+  next: function () {
+    if (Object.isNumber(this.current_page)) {
+      return this.page(++this.current_page);
+    } else {
+      return false;
+    }
+  },
+  
+  previous: function () {
+    if (Object.isNumber(this.current_page) && this.current_page > 1) {
+      return this.page(--this.current_page);
+    } else {
+      return false;
+    }
   },
   
   parseOptions: function (options) {
@@ -91,6 +125,8 @@ Gsa = Class.create({
     var uriOptions = this.searchOptions.clone();
     uriOptions.unset('onSearch');
     uriOptions.unset('onComplete');
+    if (uriOptions.get('start') == 0)
+      uriOptions.unset('start');
     var uriString = this.protocol + this.domain + '/search?' + uriOptions.toQueryString();
     return uriString;
   },
